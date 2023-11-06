66236489587a8c81a2d8ef85d53e2e6d42628e58
Artem <a.ku666@vk.com>
2023-11-06 13:57:25 +0200

    miniRepair

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import "./App.css";
 
@@ -16,17 +16,19 @@ class ShopItemClass extends React.Component {
   render() {
     return (
       <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="description">{item.descriptionFull}</div>
+        <h2>{this.props.item.brand}</h2>
+        <h1>{this.props.item.title}</h1>
+        <h3>{this.props.item.description}</h3>
+        <div className="description">{this.props.item.descriptionFull}</div>
 
         <div className="highlight-window mobile">
           <div className="highlight-overlay"></div>
         </div>
         <div className="divider"></div>
         <div className="purchase-info">
-          <div className="price">{item.currency + item.price}</div>
+          <div className="price">
+            {this.props.item.currency + this.props.item.price + ".00"}
+          </div>
           <button>Добавить в корзину</button>
         </div>
       </div>
